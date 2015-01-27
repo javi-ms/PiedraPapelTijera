052a7095f123124be710e08adf022c53b7ad2ad7
Javier <Javier@Javier>
2015-01-27 14:33:29 +0100

    1.0000002

--- src/piedrapapeltijera/Elementos.java
+++ src/piedrapapeltijera/Elementos.java
@@ -17,55 +17,78 @@ public class Elementos {
      * realizacion de piedra papel tijera pero con elementos Fuego, agua, rayo,
      * viento y tierra
      *
-     * fuego gana a viento, 
-     * viento gana a tierra, 
-     * tierra gana a rayo,
-     * rayo gano a agua,
-     * agua gana a fuego
+     * fuego gana a viento, viento gana a tierra, tierra gana a rayo, rayo gano
+     * a agua, agua gana a fuego
      *
      */
     final int FUEGO = 1;
-    final int HIELO = 2;
+    final int VIENTO = 2;
     final int RAYO = 3;
     final int TIERRA = 4;
-    final int AGUA = 4;
+    final int AGUA = 5;
+    //public int numRecogido;
 
     /**
      * variables parar recoger las jugadas
      */
     int jugadaJugador;
     int jugadaMaquina;
-    
+
     /**
-    *lo usaremos para generar la jugada aleatoria
-    */
+     * lo usaremos para generar la jugada aleatoria
+     */
     Random jugadaAleatoria = new Random();
-    
-    public String comparaElementos() {
-        
-        if () {
-            
-        }else{
-            if (true) {
-                
-            }else{
-                if (true) {
-                    
-                }else{
-                    if (true) {
-                        
-                    }
-                }
-                
+
+    int jugadaAl;
+
+    public String comparaElementos(int jugada1) {
+        jugadaJugador = jugada1;
+
+        if (jugadaJugador == FUEGO &&jugadaAl==VIENTO) {
+//
+//        } else {
+//            if (true) {
+//
+//            } else {
+//                if (true) {
+//
+//                } else {
+//                    if (true) {
+//
+//                    }
+//                }
+//            }
+//        }
+//        //return
+        }
+    }
+        void numElemento() {
+        switch (jugadaAl) {
+                case 1:
+                    System.out.println("fuego");
+
+                    break;
+                case 2:
+                    System.out.println("Viento");
+
+                    break;
+                case 3:
+                    System.out.println("Tierra");
+
+                    break;
             }
         }
-        //return
-    } 
-    void jugadaMaquina(){
-       int jugadaAl = jugadaAleatoria.nextInt(3)+1;
-        System.out.println(jugadaAl);
+        /**
+         * comparar dato1 con dato2
+         */
+
+        void jugadaMaquina() {
+        jugadaAl = jugadaAleatoria.nextInt(3) + 1;
+            System.out.println(jugadaAl);
+        }
+        /**
+         * comprobar el numero que salga se corresponde con una imagen
+         *
+         */
+
     }
-    
-    
-    
-}
